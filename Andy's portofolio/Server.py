fd628fab4b3fed6ef4bce342d9c86989453d5fe3
LRawrerL <yixuanliang05@gmail.com>
2021-07-11 19:27:22 +0800

    Initial commit
    Blank

---
+++ Andy's portofolio/Server.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+app = Flask(__name__)
+
+
+@app.route('/')
+def mainpage():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def dyn_page(page_name):
+    return render_template(page_name)
+    
